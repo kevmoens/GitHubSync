b2bdc9ee84206630a1c8bf9df72886e98d345a41
Kevin Moens <kevmoens@yahoo.com>
2023-05-18 16:35:10 -0500

    Handle -? -h to ShowHelp

--- GitHubSync/Program.cs
+++ GitHubSync/Program.cs
@@ -22,14 +22,27 @@ namespace GitHubSync
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            IConfigurationRoot config = LoadParameters(args);
+            if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "-h":
+                    case "--h":
+                    case "-?":
+                    case "--?":
+                        ShowHelp();
+                        return;
+                }
+            }
 
+            IConfigurationRoot config = LoadParameters(args);
+            var help = config.GetSection("help");
             var settings = new SyncSettings()
             {
                 Path = config["path"],
                 User = config["user"],
                 Organization = config["org"],
-                CredentialID = string.IsNullOrEmpty(config["cred"]) ? "https://github.com" :  config["cred"],
+                CredentialID = string.IsNullOrEmpty(config["cred"]) ? "https://github.com" : config["cred"],
             };
             ValidateSettings(settings);
 
@@ -60,16 +73,33 @@ namespace GitHubSync
         {
             if (string.IsNullOrEmpty(settings.Path))
             {
+                ShowHelp();
                 throw new ArgumentNullException(nameof(settings.Path));
             }
             if (string.IsNullOrEmpty(settings.User))
             {
+                ShowHelp();
                 throw new ArgumentNullException(nameof(settings.User));
             }
             if (string.IsNullOrEmpty(settings.Organization))
             {
+                ShowHelp();
                 throw new ArgumentNullException(nameof(settings.Organization));
             }
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("**GitHubSync Help**");
+            Console.WriteLine("");
+            Console.WriteLine("Use Windows Credential Manager to read in the https://github.com credentials\r\nhttps://lakecoaad-my.sharepoint.com/:w:/g/personal/kmoens_lakeco_com/EUh25H9eHOZJhQZQ3FccXiUB5hhwDaldt5BdFf2uwT1htg?e=vOjPH8");
+            Console.WriteLine("");
+            Console.WriteLine("--path = base directory that looks for sub directories that are git repositories");
+            Console.WriteLine("--org = GitHub Organization (Place holding repos)");
+            Console.WriteLine("--user = GitHub User (Cross reference to github user for permissions)");
+            Console.WriteLine("--cred = [Optional] - Name of Windows Credential Manager Setting to get the GitHub token Default is https://github.com");
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
 
         private static void RegisterServices(SyncSettings settings, ServiceCollection services)
